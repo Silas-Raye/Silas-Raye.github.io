980bfcff673b6f3bfd81683e951b275f806057ee
Silas-Raye <silas.raye@gmail.com>
2023-08-18 14:50:00 -0400

    modified:   index.html modified:   script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Dec 31, 2023 23:59:59").getTime();
+var countDownDate = new Date("Dec 31, 2022 23:59:59").getTime();
 
 var countdownfunction = setInterval(function() {
 
@@ -6,15 +6,10 @@ var countdownfunction = setInterval(function() {
     
   var distance = countDownDate - now;
     
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  var megaminutes = Math.floor(minutes / 3);
     
-  document.getElementById("days").innerHTML = days + "d ";
-  document.getElementById("hours").innerHTML = hours + "h ";
-  document.getElementById("minutes").innerHTML = minutes + "m "; 
-  document.getElementById("seconds").innerHTML = seconds + "s ";
+  document.getElementById("megaminutes").innerHTML = megaminutes + " megaminutes";
     
   if (distance < 0) {
     clearInterval(countdownfunction);
