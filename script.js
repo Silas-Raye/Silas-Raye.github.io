901874ab3010116a6b4fa9f2b66bc9baf7ac8a4c
Silas-Raye <silas.raye@gmail.com>
2023-08-18 14:57:01 -0400

    modified:   script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var countdownfunction = setInterval(function() {
   var totalMinutes = Math.floor(distance / (1000 * 60));
   var megaminutes = Math.floor(totalMinutes / 3);
     
-  document.getElementById("megaminutes").innerHTML = megaminutes + " megaminutes";
+  document.getElementById("megaminutes").innerHTML = megaminutes + " megaminutes till I see you ❤️";
     
   if (distance < 0) {
     clearInterval(countdownfunction);
